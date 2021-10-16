0af93777ce93f907c2d823d4a62d3ef2788a55d2
AndrewIndeche <indecheandrew@gmail.com>
2021-10-16 17:06:16 +0300

    add follow,comment,post models

--- models.py
+++ models.py
@@ -1,3 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 class Profile(models.Model):
     '''
     model class for User Profile
@@ -6,4 +11,91 @@ class Profile(models.Model):
     name = models.Charfield(blank=True,max_length=60)
     bio = models.TextField(blank=True,max_length=500)
     location = models.CharField(max_length=60, blank=True) models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    profile =
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def search_profile(cls, name):
+        return cls.objects.filter(user__username__icontains=name).all()
+
+
+class Follow(models.Model):
+    follower = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='following')
+    followed = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='followers')
+
+    def __str__(self):
+        return f'{self.follower} Follow'
+
+
+class Comment(models.Model):
+    ''' a model for comments'''
+    related_post = models.ForeignKey('Image', on_delete=models.CASCADE)
+    name = models.ForeignKey('Profile', on_delete=models.CASCADE)
+    comment_body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def save_comments(self):
+        ''' method to save comment instance '''
+        self.save()
+
+    def delete_comment(self):
+        '''method to delete comment instance '''
+        self.delete()
+
+    def edit_comment(self, new_comment):
+        ''' method to edit a comment '''
+        self.comment_body = new_comment
+        self.save()
+
+    def __str__(self):
+        return f'Comment by {self.name}'
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='posts/')
+    name = models.CharField(max_length=250, blank=True)
+    caption = models.CharField(max_length=250, blank=True)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        ordering = ["?"]
+
+    def get_absolute_url(self):
+        return f"/post/{self.id}"
+
+    @property
+    def get_all_comments(self):
+        return self.comments.all()
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def total_likes(self):
+        return self.likes.count()
+
+    def __str__(self):
+        return f'{self.user.name} Post'

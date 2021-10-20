5f41e8488e2e58d772e608479a615b6190014e39
AndrewIndeche <indecheandrew@gmail.com>
2021-10-20 10:40:34 +0300

    add styling to login page

--- insta/urls.py
+++ insta/urls.py
@@ -3,10 +3,13 @@ from . import views
 from django.conf.urls import url
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import include
 
 urlpatterns = [
 
-    url(r'^$',views.signup,name = 'signup'),
+    url('signup/', views.signup, name='signup'),
+    url('/', include('django.contrib.auth.urls')),
+    url(r'^$',views.index,name = 'index'),
     url(r'^profile/$', views.profile, name='profile'),
     url(r'new/image$', views.new_image, name='new_image'),
     url(r'comments/(\d+)/', views.comments, name='comments'),
